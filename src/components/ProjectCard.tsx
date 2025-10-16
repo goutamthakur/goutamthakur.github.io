475190b65612e4ae086e149822f3fe7f784cfd12
goutamthakur <goutamthakur2508@gmail.com>
2025-10-16 23:18:33 +0530

    fix: image in project card made responsive

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -6,20 +6,26 @@ interface ProjectCardProps {
 
 const ProjectCard = ({ project }: ProjectCardProps) => {
   return (
-    <div className="card p-6 relative overflow-hidden h-106">
-      <h1 className="card-h1 md:text-2xl font-semibold">{project.heading}</h1>
+    <div className="card p-6 overflow-hidden flex flex-col justify-between h-full">
+      <div>
+        <h1 className="card-h1 md:text-2xl font-semibold">{project.heading}</h1>
 
-      <div className="my-2 md:my-4 w-4/6 text-wrap">
-        <p className="my-4">{project.desc}</p>
+        <div className="my-2 md:my-4 w-4/6 text-wrap">
+          <p className="my-4">{project.desc}</p>
+        </div>
+
+        <div className="my-2 md:my-4 w-4/6 text-wrap">
+          <Tags items={project.tags} />
+        </div>
       </div>
 
-      <Tags items={project.tags} />
-
-      <img
-        src={project.image}
-        alt={project.heading}
-        className="absolute bottom-0 right-0 block object-cover rounded-lg w-80"
-      />
+      <div className="m-6 -mb-6 w-full rounded-tl-2xl overflow-hidden flex-shrink-0">
+        <img
+          src={project.image}
+          alt={project.heading}
+          className="w-full h-64 md:h-80 object-cover object-bottom-right"
+        />
+      </div>
     </div>
   );
 };
